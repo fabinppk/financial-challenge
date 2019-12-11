ba49dde39838708a25034b70ed2fc9f33be1a6c6
Fabiano <fabinppk@hotmail.com>
2019-12-11 11:59:01 -0300

    feat: reading store from component

--- src/components/organisms/LeftSide/index.js
+++ src/components/organisms/LeftSide/index.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
+import actions from '_redux/actions';
 import CustomSlider from '_molecules/CustomSlider';
 import style from '_organisms/LeftSide/index.css';
+import { grossCalculator, taxCalculator } from '_utils/coreCalculator';
 
-const LeftSide = () => (
-    <div className={style.blockLeft}>
-        <p className={style.subtitle}>First deposit</p>
-        <span className={style.value}>R$ 1.000</span>
-        <div>
-            <button type="button" className={classNames(style.button, style.minus)}>
-                <span className={style.symbol} />
-            </button>
-            <button type="button" className={classNames(style.button, style.plus)}>
-                <span className={style.symbol} />
-            </button>
+const LeftSide = () => {
+    const { financial } = useSelector(state => ({
+        ...state
+    }));
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        async function calculate() {
+            const gross = await grossCalculator(
+                financial.initialAmount,
+                financial.interest,
+                financial.period
+            );
+            dispatch(actions.setGross(gross));
+        }
+
+        calculate();
+    }, [financial.period]);
+
+    useEffect(() => {
+        async function calculate() {
+            const tax = await taxCalculator(
+                financial.initialAmount - financial.gross,
+                financial.period
+            );
+            dispatch(actions.setTax(tax));
+        }
+
+        calculate();
+    }, [financial.gross, financial.period]);
+
+    return (
+        <div className={style.blockLeft}>
+            <p className={style.subtitle}>First deposit</p>
+            <span className={style.value}>R$ {financial.initialAmount}</span>
+            <div>
+                <button type="button" className={classNames(style.button, style.minus)}>
+                    <span className={style.symbol} />
+                </button>
+                <button type="button" className={classNames(style.button, style.plus)}>
+                    <span className={style.symbol} />
+                </button>
+            </div>
+            <p className={style.monthSubTitle}>For</p>
+            <p className={style.month}>{financial.period} months</p>
+            <div className={style.containerSlide}>
+                <CustomSlider />
+            </div>
         </div>
-        <p className={style.monthSubTitle}>For</p>
-        <p className={style.month}>60 months</p>
-        <div className={style.containerSlide}>
-            <CustomSlider />
-        </div>
-    </div>
-);
+    );
+};
 
 export default LeftSide;
--- src/components/organisms/RightSide/index.js
+++ src/components/organisms/RightSide/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import style from '_organisms/RightSide/index.css';
 import relaxing from '_images/relaxing.png';
 
-const RightSide = () => (
-    <div className={style.blockRight}>
-        <img className={style.relaxing} src={relaxing} alt="relaxing" />
-        <p>after 60 months you would have</p>
-        <p className={style.amount}>R$ 1296,15</p>
-        <p>(gross amount R$ 1348,41)</p>
-        <p className={style.ps}>
-            {`This estimate does not constitute a guarantee of future earnings. It
+const RightSide = () => {
+    const { financial } = useSelector(state => ({
+        ...state
+    }));
+
+    return (
+        <div className={style.blockRight}>
+            <img className={style.relaxing} src={relaxing} alt="relaxing" />
+            <p>after {financial.period} months you would have</p>
+            <p className={style.amount}>
+                tax: {financial.tax.toFixed(2)}
+                <br />
+                R$ {parseFloat(financial.gross - financial.tax).toFixed(2)}
+            </p>
+            <p>(gross amount R$ {parseFloat(financial.gross).toFixed(2)})</p>
+            <p className={style.ps}>
+                {`This estimate does not constitute a guarantee of future earnings. It
             is only an estimate based on today's interbank Deposit rate for the
             entire period that your deposit would remain in your NuConta
             account, and considering no withdrawals.`}
-        </p>
-    </div>
-);
+            </p>
+        </div>
+    );
+};
 
 export default RightSide;
