7452a850c7dbf1a956b9bcdd6d18886bdfa81c98
Fabiano <fabinppk@hotmail.com>
2019-12-11 11:58:17 -0300

    feat: create core calculator functions

---
+++ src/utils/coreCalculator.js
@@ -0,0 +1,17 @@
+export const grossCalculator = async (initialAmount, interest, period) => {
+    const gross = await (initialAmount * Math.pow(1 + interest / 100, period));
+    return gross;
+};
+
+export const taxCalculator = (gross, period) => {
+    let tax;
+
+    if (period <= 6) tax = -gross * 0.225;
+    if (period > 6 && period <= 12) tax = -gross * 0.2;
+    if (period > 12 && period <= 18) tax = -gross * 0.175;
+    if (period > 18) tax = -gross * 0.15;
+
+    return tax;
+};
+
+export default { grossCalculator, taxCalculator };
