98685a657859555278bf44e8b432e1751befeeb3
Fabiano <fabinppk@hotmail.com>
2019-12-14 15:58:38 -0300

    fix: dotenv warning

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-require('dotenv').config();
 const withPlugins = require('next-compose-plugins');
 const css = require('@zeit/next-css');
 const images = require('next-images');
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
 
 const webpackExtra = require('./webpack.extra');
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
@@ -19,10 +16,6 @@ const nextConfiguration = {
         config.plugins.push(
             new FilterWarningsPlugin({
                 exclude: /mini-css-extract-plugin[^]*Conflicting order between:/
-            }),
-            new Dotenv({
-                path: path.join(__dirname, '.env'),
-                systemvars: true
             })
         );
         config.resolve.alias = {
